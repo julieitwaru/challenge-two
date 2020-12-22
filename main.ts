a7b772340cd382079ddc3a7660a093d7b88ad066
julieitwaru <72779833+julieitwaru@users.noreply.github.com>
2020-12-22 10:23:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+while True:
+
+    #if light level is greater than 15, then set neopixels of
+    print("light level:" + input.light_level())
+    if input.light_level() > 15:
+        light.clear()
+
+    #if light level is equal to or greater than 6, then set neopixels blue
+    elif input.light_level() >= 6
+        light.set_all(color.rgb(0,0,255))
+
+    else:
+        light.set_all(color.rgb(255,165,0))
